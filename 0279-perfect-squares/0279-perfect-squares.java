75b7bb26654b8d7be0a9bdc8ca37908589d766bb
Abhishek Singh <as4688541@gmail.com>
2026-09-22 22:58:18 +0530

    Time: 194 ms (12.12%), Space: 44.7 MB (42.65%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPerfectSquare(int n){
+        int sqrt = (int)Math.sqrt(n);
+        return (sqrt*sqrt == n);
+    }
+
+    public int helper(int n , int[] dp){
+        // Base Case
+         if(isPerfectSquare(n))return 1;
+
+         if(dp[n] != -1)return dp[n];
+
+         int min = Integer.MAX_VALUE;
+
+         for(int i=1; i*i<=n; i++){
+
+            int count = helper(i*i,dp) + helper(n - (i*i),dp);
+
+            min = Math.min(min,count);
+        }
+        return dp[n] = min;
+        
+    }
+
+    
+    public int numSquares(int n) {
+ 
+        // for n = a + b + c +....(a,b,c are perfect squares)
+        // try all combination 
+
+        // base case
+        // memoization 
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n , dp);
+    }
+}
+
+       
