2ea7bbb65e1649e5729d1ea78c8ab44f007b84c6
Abhishek Singh <as4688541@gmail.com>
2026-09-22 23:07:40 +0530

    Time: 26 ms (82.16%), Space: 44.4 MB (67.19%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,40 +1,36 @@
 class Solution {
-    public boolean isPerfectSquare(int n){
+    public boolean isPerfect(int n){
         int sqrt = (int)Math.sqrt(n);
         return (sqrt*sqrt == n);
     }
 
-    public int helper(int n , int[] dp){
-        // Base Case
-         if(isPerfectSquare(n))return 1;
+    public int numSquares(int n) {
+        // tabulization
+     
+        
 
-         if(dp[n] != -1)return dp[n];
+        if(n == 1 || n == 2)return n;
 
-         int min = Integer.MAX_VALUE;
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-         for(int i=1; i*i<=n; i++){
+        
 
-            int count = helper(i*i,dp) + helper(n - (i*i),dp);
-
-            min = Math.min(min,count);
+        for(int i=3; i<=n; i++){
+            if(isPerfect(i)){
+                dp[i] = 1;
+                continue;
+            }
+            int min = Integer.MAX_VALUE;
+               for(int j=1; j*j<=i; j++){
+                int count = dp[j*j] + dp[i - j*j];
+                min = Math.min(min,count);
+               }
+               dp[i] = min;
         }
-        return dp[n] = min;
+        return dp[n];
         
     }
-
-    
-    public int numSquares(int n) {
- 
-        // for n = a + b + c +....(a,b,c are perfect squares)
-        // try all combination 
-
-        // base case
-        // memoization 
-
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n , dp);
-    }
 }
-
-       
